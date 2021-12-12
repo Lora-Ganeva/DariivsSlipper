eb06c5dbb33c188c38f99a83f4086ead43351cfd
QATest <QATest@DESKTOP-80J0OT9>
2021-12-12 16:19:46 +0200

    Add project files.

---
+++ DariivsSleeper/Battle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace DariivsSleeper.cs
+{
+    class Battle
+    {
+
+        public static void StartFight(Warrior warrior1, Warrior warrior2) 
+        {
+            while (true)
+            {
+                if (GetAttackResult(warrior1, warrior2)  ==  "GameOver")
+                {
+                    Console.WriteLine("GameOver");
+                    break;  
+
+                }
+
+                if (GetAttackResult(warrior2, warrior1)  ==  "GameOver")
+                {
+                    Console.WriteLine("GameOver");
+                    break;
+
+                }
+            }
+        
+        }
+
+        public static string GetAttackResult(Warrior warriorA, Warrior warriorB)
+        {
+            double warAAttkAmt = warriorA.Attack();
+            double warBBlkAmt = warriorB.Block();
+
+            double dmg2WarB = warAAttkAmt - warBBlkAmt;
+
+            if(dmg2WarB > 0)
+            {
+                warriorB.Health = warriorB.Health - dmg2WarB;
+            } 
+            else dmg2WarB = 0;  
+            
+            Console.WriteLine("{0} Attacks {1} and Deals {2} Damage", 
+                warriorA.Name, 
+                warriorB.Name, 
+                dmg2WarB);
+
+            Console.WriteLine("{0} Has {1} Health\n",
+                warriorB.Name,
+                warriorB.Health);
+
+            if (warriorB.Health <= 0)
+            {
+                Console.WriteLine("{0} has Died and {1} is Victorius\n",
+                    warriorB.Name,
+                    warriorA.Name);
+                return "GameOver";
+            }
+            else return "Fight Again";
+        }
+    }
+}
---
+++ DariivsSleeper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DariivsSleeper.cs
+{
+    class Program 
+    {
+
+
+        static void Main(string[] args)
+        {
+            Warrior Darii = new Warrior("Darii", 2000, 240, 50);
+
+            Warrior Slipper = new Warrior("Slipper", 2000, 240, 50);  
+            
+            Battle.StartFight(Darii, Slipper);
+
+            Console.ReadLine();
+        }
+    
+    
+    }
+}
